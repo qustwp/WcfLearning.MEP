c072f360fbc6f31e5a010c472831c8c44a8e0492
qustwp <695209671@qq.com>
2017-08-24 20:44:17 +0800

    WcfLearn.MEP 初始项目

---
+++ WcfLearning.MEP.Contract/CalculatorService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WcfLearning.MEP.Contract
+{
+    public class CalculatorService:ICalculatorService
+    {
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Divide(int a, int b)
+        {
+            if (b == 0) throw new Exception("");
+
+            return a / b;
+        }
+    }
+}
---
+++ WcfLearning.MEP.Contract/ICalculatorService.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+
+namespace WcfLearning.MEP.Contract
+{
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+        [OperationContract]
+        int Multiply(int a, int b);
+
+        [OperationContract]
+        double Divide(int a, int b);
+    }
+}
